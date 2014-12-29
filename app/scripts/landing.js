434a41ee2e256e23474435e772afad2e739db361
heather <hlahaie@gmail.com>
2014-12-28 18:31:18 -0700

    Added jQuery animations to landing page

---
+++ app/scripts/landing.js
@@ -0,0 +1,17 @@
+$(document).ready(function() { 
+  $('.hero-content h3').click(function(){
+      console.log("hello!");
+      var subText = $(this).text();
+      $(this).text(subText + "!");
+  });
+
+  var onHoverAction = function(event) {
+    console.log('Hover action triggered.');
+    $(this).animate({'margin top': '10px'});
+  };
+  var offHoverAction = function(event) {
+    console.log('off-hover action triggered.');
+    $(this).animate({'margin top': '0px'});
+  };
+  $('.selling-points .point').hover(onHoverAction, offHoverAction);
+});
