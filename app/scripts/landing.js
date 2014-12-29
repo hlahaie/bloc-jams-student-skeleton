2894710ce8b0ce07ca70e20021d7223fdcffcfb3
heather <hlahaie@gmail.com>
2014-12-28 18:38:42 -0700

    Commented console.log lines

--- app/scripts/landing.js
+++ app/scripts/landing.js
@@ -1,16 +1,16 @@
 $(document).ready(function() { 
   $('.hero-content h3').click(function(){
-      console.log("hello!");
+      // console.log("hello!");
       var subText = $(this).text();
       $(this).text(subText + "!");
   });
 
   var onHoverAction = function(event) {
-    console.log('Hover action triggered.');
+    // console.log('Hover action triggered.');
     $(this).animate({'margin top': '10px'});
   };
   var offHoverAction = function(event) {
-    console.log('off-hover action triggered.');
+    // console.log('off-hover action triggered.');
     $(this).animate({'margin top': '0px'});
   };
   $('.selling-points .point').hover(onHoverAction, offHoverAction);
