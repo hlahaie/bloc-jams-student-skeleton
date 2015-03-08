3cfc2a8d5a5366a727d9b741e7455bb8dc3afccf
heather <hlahaie@gmail.com>
2015-03-07 22:49:54 -0700

    Changed slider to update (music location) data on change

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,11 +12,11 @@ var albumPicasso = {
   albumArtUrl: '/images/album-placeholder.png',
  
   songs: [
-    { name: 'Blue', length: '4:26', audioUrl: '/music/placeholders/blue' },
-    { name: 'Green', length: '3:14', audioUrl: '/music/placeholders/green' },
-    { name: 'Red', length: '5:01', audioUrl: '/music/placeholders/red' },
-    { name: 'Pink', length: '3:21', audioUrl: '/music/placeholders/pink' },
-    { name: 'Magenta', length: '2:15', audioUrl: '/music/placeholders/magenta' }
+      { name: 'Blue', length: 163.38, audioUrl: '/music/placeholders/blue' },
+      { name: 'Green', length: 105.66 , audioUrl: '/music/placeholders/green' },
+      { name: 'Red', length: 270.14, audioUrl: '/music/placeholders/red' },
+      { name: 'Pink', length: 154.81, audioUrl: '/music/placeholders/pink' },
+      { name: 'Magenta', length: 375.92, audioUrl: '/music/placeholders/magenta' }
   ]
 };
 
@@ -66,12 +66,13 @@ blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider,
 blocJams.controller('Collection.controller', ['$scope','SongPlayer', function($scope, SongPlayer) {
   $scope.albums = [];
     for (var i = 0; i < 33; i++) {
-    $scope.albums.push(angular.copy(albumPicasso));
-   }
+      $scope.albums.push(angular.copy(albumPicasso));
+    }
   $scope.playAlbum = function(album){
     SongPlayer.setSong(album, album.songs[0]); // Targets first song in the array.
   }
 }]);
+
 blocJams.controller('Album.controller', ['$scope', 'SongPlayer', function($scope, SongPlayer) {
   $scope.album = angular.copy(albumPicasso);
     var hoveredSong = null;
@@ -127,38 +128,47 @@ blocJams.service('SongPlayer', function() {
       this.playing = false;
         currentSoundFile.pause();
     },
-      next: function() {
-        var currentTrackIndex = trackIndex(this.currentAlbum, this.currentSong);
-        currentTrackIndex++;
-        if (currentTrackIndex >= this.currentAlbum.songs.length) {
-          currentTrackIndex = 0;
-        }
-        var song = this.currentAlbum.songs[currentTrackIndex];
-        this.setSong(this.currentAlbum, song);
-        this.currentSong = this.currentAlbum.songs[currentTrackIndex];
-      },
-      previous: function() {
-        var currentTrackIndex = trackIndex(this.currentAlbum, this.currentSong);
-        currentTrackIndex--;
-        if (currentTrackIndex < 0) {
-          currentTrackIndex = this.currentAlbum.songs.length - 1;
-        }
-        var song = this.currentAlbum.songs[currentTrackIndex];
-        this.setSong(this.currentAlbum, song);
-        this.currentSong = this.currentAlbum.songs[currentTrackIndex];
-      },
+    next: function() {
+      var currentTrackIndex = trackIndex(this.currentAlbum, this.currentSong);
+      currentTrackIndex++;
+      if (currentTrackIndex >= this.currentAlbum.songs.length) {
+        currentTrackIndex = 0;
+      }
+      var song = this.currentAlbum.songs[currentTrackIndex];
+      this.setSong(this.currentAlbum, song);
+      this.currentSong = this.currentAlbum.songs[currentTrackIndex];
+    },
+    previous: function() {
+      var currentTrackIndex = trackIndex(this.currentAlbum, this.currentSong);
+      currentTrackIndex--;
+      if (currentTrackIndex < 0) {
+        currentTrackIndex = this.currentAlbum.songs.length - 1;
+      }
+      var song = this.currentAlbum.songs[currentTrackIndex];
+      this.setSong(this.currentAlbum, song);
+      this.currentSong = this.currentAlbum.songs[currentTrackIndex];
+    },
+    seek: function(time) {
+      // Checks to make sure that a sound file is playing before seeking.
+      if(currentSoundFile) {
+        // Uses a Buzz method to set the time of the song.
+        currentSoundFile.setTime(time);
+      }
+    },
+
     setSong: function(album, song) {
       if (currentSoundFile) {
-      currentSoundFile.stop();
+        currentSoundFile.stop();
       }
       this.currentAlbum = album;
       this.currentSong = song;
       currentSoundFile = new buzz.sound(song.audioUrl, {
-      formats: [ "mp3" ],
-      preload: true
-    });
+        formats: [ "mp3" ],
+        preload: true
+      });
  
-    this.play();
+      this.play();
+
     }
   };
 });
@@ -174,21 +184,48 @@ blocJams.directive('slider', ['$document', function($document){
     offsetXPercent = Math.min(1, offsetXPercent);
     return offsetXPercent;
   }
+
+  var numberFromValue = function(value, defaultValue) {
+    if (typeof value === 'number') {
+       return value;
+    }
+ 
+    if(typeof value === 'undefined') {
+      return defaultValue;
+    }
+ 
+    if(typeof value === 'string') {
+      return Number(value);
+    }
+  }
  
   return {
     templateUrl: '/templates/directives/slider.html', // We'll create these files shortly.
     replace: true,
     restrict: 'E',
-      scope: {}, // Creates a scope that exists only in this directive.
+      scope: {
+        onChange: '&'
+      }, // Creates a scope that exists only in this directive.
+
     link: function(scope, element, attributes) {
       // These values represent the progress into the song/volume bar, and its max value.
       // For now, we're supplying arbitrary initial and max values.
       scope.value = 0;
-      scope.max = 200;
-    var $seekbar = $(element);
+      //Switch to a more sensible default max
+      scope.max = 100;
+      var $seekbar = $(element);
+        attributes.$observe('value', function(newValue) {
+          scope.value = numberFromValue(newValue, 0);
+        });
+   
+        attributes.$observe('max', function(newValue) {
+          scope.max = numberFromValue(newValue, 100) || 100;
+        });
   
       var percentString = function () {
-        var percent = Number(scope.value) / Number(scope.max) * 100;
+        var value = scope.value || 0;
+        var max = scope.max || 100;
+        percent = value / max * 100;
         return percent + "%";
       }
  
@@ -199,27 +236,42 @@ blocJams.directive('slider', ['$document', function($document){
       scope.thumbStyle = function() {
         return {left: percentString()};
       }
+      // var notifyCallback = function(newValue) {
+      //   if(typeof scope.onChange === 'function') {
+      //     scope.onChange({value: newValue});
+      //   }
+      // };
+      scope.onClickSlider = function(event) {
+        var percent = calculateSliderPercentFromMouseEvent($seekbar, event);
+        scope.value = percent * scope.max;
+          notifyCallback(scope.value);
+      }
+
+      scope.trackThumb = function() {
+        $document.bind('mousemove.thumb', function(event){
+          var percent = calculateSliderPercentFromMouseEvent($seekbar, event);
+          scope.$apply(function(){
+            scope.value = percent * scope.max;
+              notifyCallback(scope.value);
+          })
+        });
+
+        // cleanup
+        $document.bind('mouseup.thumb', function(){
+          $document.unbind('mousemove.thumb');
+          $document.unbind('mouseup.thumb');
+        });
+      };
+      var notifyCallback = function(newValue) {
+        if(typeof scope.onChange === 'function') {
+          scope.onChange({value: newValue});
+        }
+      };
+
+
     }
   };
 }]);
 
-scope.onClickSlider = function(event) {
-  var percent = calculateSliderPercentFromMouseEvent($seekbar, event);
-  scope.value = percent * scope.max;
-}
 
-scope.trackThumb = function() {
-  $document.bind('mousemove.thumb', function(event){
-    var percent = calculateSliderPercentFromMouseEvent($seekbar, event);
-    scope.$apply(function(){
-      scope.value = percent * scope.max;
-    })
-  });
-
-  cleanup
-  $document.bind('mouseup.thumb', function(){
-    $document.unbind('mousemove.thumb');
-    $document.unbind('mouseup.thumb');
-  });
-};
 
